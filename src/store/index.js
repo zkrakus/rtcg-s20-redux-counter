f4972f51037a7b805f02c9b9d4ab7d4f8330dd44
zkrakus <zkrakus@gmail.com>
2025-05-30 15:48:09 -0400

    feat:s20-l315 added toggleCounter handle and added it to store reducer.

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,33 @@
 import { createStore } from "redux";
 
-const counterReducer = (store = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (store = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: store.counter + 1,
+      showCounter: store.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
       counter: store.counter - 1,
+      showCounter: store.showCounter,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: store.counter + action.amount,
+      showCounter: store.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      counter: store.counter,
+      showCounter: !store.showCounter,
     };
   }
 
